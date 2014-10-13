3ad310bea3eedc6e79dd0c1ebaa864623fd44fb5
Zlatko Gudasić <zlatko.gudasic@gmail.com>
2014-10-13 07:34:59 +0200

    popravio instalaciju testova

--- install_tests.sql
+++ install_tests.sql
@@ -8,7 +8,7 @@ SET TERMOUT ON
 @@src/tests/ut_naplata_po_namjeni_ot.pks
 @@src/tests/ut_strategija_naplate_ot.pks
 @@src/tests/ut_uplate_queue_pkg.pks
-@@src/tests/ut_prioriteti_naplate.pks
+@@src/tests/ut_prioriteti_naplate_pkg.pks
 @@src/tests/ut_app_pkg.pks
 @@src/tests/ut_sms_callback.pks
 @@src/tests/ut_regulator_check_callback.pks
@@ -20,7 +20,7 @@ SET TERMOUT ON
 @@src/tests/ut_naplata_po_namjeni_ot.pkb
 @@src/tests/ut_strategija_naplate_ot.pkb
 @@src/tests/ut_uplate_queue_pkg.pkb
-@@src/tests/ut_prioriteti_naplate.pkb
+@@src/tests/ut_prioriteti_naplate_pkg.pks
 @@src/tests/ut_app_pkg.pkb
 @@src/tests/ut_sms_callback.pkb
 @@src/tests/ut_regulator_check_callback.pks
